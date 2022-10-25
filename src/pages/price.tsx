379fb8e7a42646936320ea53d0f95e5048aaa183
andreytretiachenko <andrey.tretiachenko@gmail.com>
2022-10-26 01:14:24 +0300

    222

--- src/pages/price.tsx
+++ src/pages/price.tsx
@@ -2,11 +2,11 @@ import React from "react"
 
 export default function Price() {
   return (
-    <div className='container'>
-        <div className='row'>
+    <div className='container px-4'>
+        <div className='row g-4'>
             <div className='col'>
                 <div>
-                    <input type='text' className="form" list='data-price'/>
+                    <input type='text' className="g-4" list='data-price'/>
                     <datalist id='data-price'>
                         <option>123</option>
                         <option>321</option>
@@ -14,7 +14,7 @@ export default function Price() {
                         <option>111</option>
                         <option>333</option>
                     </datalist>
-                    <button type='button' className="btn btn-primary">add</button>
+                    <button type='button' className="btn btn-primary btn-py-1 btn-px-1">add</button>
                 </div>
             </div>
             <div className='col'>
