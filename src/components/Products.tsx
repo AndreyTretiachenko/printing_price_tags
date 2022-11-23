0bbc38530a25c7b7d7ff92b391c74cc4f7bfb24a
на работе <andrey.tretiachenko@gmail.com>
2022-11-23 19:14:35 +0300

    create find

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -9,6 +9,7 @@ import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 
 export default function Products() {
   const [category, setCategory] = useState("");
+  const [findroduct, setFindProduct] = useState("")
   const { productListModel, products, categoryList, defaultProduct } = useAppSelector(
     (state) => state.products
   );
@@ -17,6 +18,21 @@ export default function Products() {
   );
   const [value, setValue] = useState<string>("");
   const dispatch = useAppDispatch();
+
+  const handlerFindProduct = (findText:string) => {
+    fetch(`http://service.dvinahome.ru/findproduct?text="${findText}"`, {
+      method: "GET",
+      headers: { Authorization: "basic dXNlcjpwYXNz" },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        dispatch(loadProducts(result["data"]));
+        dispatch(setProductListModel(result["data"]));
+        dispatch(setDefaultProduct(result["data"][0][0]));
+      })
+  }
 
   const hadlerGetProduct = (value: string) => {
     fetch(`http://service.dvinahome.ru/?category="${value}"`, {
@@ -80,8 +96,9 @@ export default function Products() {
   }
 
   return (
-    <div className="d-block justify-content-start my-3">
-      <div style={{ paddingBottom: 10 }}>
+    <div>
+      <div style={{marginBottom:10}}>
+      <div style={{ paddingBottom: 10, display:'inline' }}>
         <label>выберите категорию товара:&nbsp;</label>
         <div style={{ display: "inline" }}>
           <select
@@ -100,6 +117,20 @@ export default function Products() {
             загрузить
           </button>
         </div>
+      </div>
+      <div style={{display:'inline'}}>
+        <label>поиск по названию товара:&nbsp;</label>
+        <input list='list' type={'text'} 
+        value={findroduct}
+        onChange={(e) => setFindProduct(e.target.value)}
+        />
+        <button
+            className="btn btn-sm btn-primary mx-3"
+            onClick={() => {handlerFindProduct(findroduct)}}
+        >
+            поиск
+        </button>
+      </div>
       </div>
       <div>
         <label>выберите название товара:&nbsp;</label>
